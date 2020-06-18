1262189abf2a0a624f667df8df2dc687941c07a6
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2020-06-18 02:25:35 -0300

    added complementary challenge 8

---
+++ desafios/complementarios/condicionales/desafio8_pulsaciones.py
@@ -0,0 +1,8 @@
+# Desafio 8
+# Calcular el número de pulsaciones que una persona debe tener por cada 10 segundos de ejercicio, 
+# si la fórmula es: Número de pulsaciones = (220 - edad)/10
+
+
+edad = int(input("Edad: "))
+
+print(f"Cada 10 segundos de ejercicio sus pulsaciones deben ser de {(220-edad)/10}")
