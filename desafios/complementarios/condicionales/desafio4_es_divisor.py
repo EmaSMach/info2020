a24215658e917d6ee41ab37ac11e0c66ae77a4b3
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2020-06-18 01:11:43 -0300

    added level 1, challenge 4

---
+++ desafios/complementarios/condicionales/desafio4_es_divisor.py
@@ -0,0 +1,10 @@
+# Realizar un programa que sea capaz de, habiéndose ingresado dos números m y n, determine si n es divisor de m.
+
+
+num1 = int(input("Primer número: "))
+num2 = int(input("Segundo número: "))
+
+if num1 % num2 == 0:
+    print(f"{num2} es divisor de {num1}")
+else:
+    print(f"{num2} no es divisor de {num1}")
